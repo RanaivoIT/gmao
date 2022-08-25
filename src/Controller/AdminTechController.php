67f73b08784b6940c52080831e52f37cb0dc934c
Micah <ranaivo.it@gmail.com>
2022-08-25 18:06:50 +0300

    Mettre à jours techtype

--- src/Controller/AdminTechController.php
+++ src/Controller/AdminTechController.php
@@ -5,12 +5,20 @@ namespace App\Controller;
 use App\Entity\Tech;
 use App\Form\TechType;
 use App\Repository\TechRepository;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminTechController extends AbstractController
 {
+    private $manager;
+    public function __constructor(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
     #[Route('/admin/tech', name: 'admin_tech')]
     public function index(TechRepository $techrepo): Response
     {
@@ -24,10 +32,25 @@ class AdminTechController extends AbstractController
     }
 
     #[Route('/admin/tech/add', name: 'admin_tech_add')]
-    public function add(): Response
+    public function add(Request $request): Response
     {
         $tech = new Tech();
         $form = $this->createForm(TechType::class, $tech);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($tech);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Le nouveau technicien <strong>'"+ $tech.firstname + " " + $tech.lastname +"'<strong> est ajouté !!!"
+            );
+
+            return $this->redirectToRoute('admin_tech_show', [
+                'id' => $tech->getId()
+            ]);
+        }
 
         return $this->render('admin/tech/form.html.twig', [
             'title' => 'Admin - Tech',
--- src/Form/TechType.php
+++ src/Form/TechType.php
@@ -6,20 +6,23 @@ use App\Entity\Tech;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class TechType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('avatar')
+            ->add('avatar', FileType::class)
             ->add('firstname')
             ->add('lastname')
             ->add('speciality')
             ->add('address')
             ->add('contact')
-            ->add('email')
-            ->add('hash')
+            ->add('email', EmailType::class)
+            ->add('hash', PasswordType::class)
         ;
     }
 
